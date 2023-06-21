dae1b6702ee202dfb57acab323110bb3b0673c46
nadous <114181724+enqilin@users.noreply.github.com>
2023-06-21 08:43:34 +0000

    fichero 7

---
+++ pregunta7.py
@@ -0,0 +1,32 @@
+"""Deberá generar 2000 Armaduras siguiendo el formato de la imagen del primer ejercicio contemplando las siguientes legiones FL, TF, TK, CT, FN, FO y los dígitos generados de manera aleatoria;
+
+deberá cargar los Armaduras generados en dos tablas hash encadenadas, en la primera se deberá agrupar de acuerdo con los tres últimos dígitos del código y en la segunda a partir de las iniciales de la legión;
+"""
+
+
+import random
+
+class nodoArmadura(object):
+    def __init__(self,info):
+        self.info = info
+        self.sig = None
+
+class nodoLegion(object):
+    def __init__(self,info):
+        self.info = info
+        self.sig = None
+
+class Armadura(object):
+    def __init__(self):
+        sef.inicio = None
+        self.tamanio = 0
+
+    def 
+
+
+
+
+if __name__=='__main__':
+    a = Armadura()
+    
+
