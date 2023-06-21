1893a98ec6093837c39a984fbd65441d0bd6254f
nadous <114181724+enqilin@users.noreply.github.com>
2023-06-21 08:12:30 +0000

    cambios

---
+++ pregunta9.py
@@ -0,0 +1,16 @@
+
+"""
+Generar un grafo no dirigido con planetas del MCU y diseñar los algoritmos necesarios para resolver las siguientes actividades:
+
+ 
+
+los siguientes planetas deben estar en el grafo: Tierra,Knowhere,Zen-Whoberi,Vomir,Titán,Nidavellir, agregue 7 más;
+ 
+
+genere al menos 4 aristas para cada uno de los planetas del grafo, no puede haber nodos con arcos a sí mismo;
+ 
+
+encuentre el árbol de expansión mínima en cuanto a costos para recorrer todos los planetas
+
+"""
+
